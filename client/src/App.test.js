c10ef954d05a52e3a2220fc94047196988ecb443
chamoo334 <moore.cmarie@gmail.com>
2022-04-01 11:33:05 -0700

    updated client and travis yml to incorporate react tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,5 +1,29 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { configure, shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+configure({ adapter: new Adapter() });
 
-test('renders learn react link', () => {});
+// removing render warning
+const originalError = console.error;
+beforeAll(() => {
+  console.error = (...args) => {
+    if (
+      /Warning: ReactDOM.render is no longer supported in React 18./.test(
+        args[0]
+      )
+    ) {
+      return;
+    }
+    originalError.call(console, ...args);
+  };
+});
+
+afterAll(() => {
+  console.error = originalError;
+});
+
+it('renders without crashing', () => {
+  shallow(<App />);
+});
