ab2efb66354b594d235569988957c99df28b22b6
chamoo334 <moore.cmarie@gmail.com>
2022-04-09 16:15:07 -0700

    initial server and mongoose schema test setup

--- server/app.test.js
+++ server/app.test.js
@@ -1,32 +1,53 @@
 import request from 'supertest';
-import app from './app.js';
+const createApp = require('./app.js');
+const connectDB = require('./config/db');
 
-describe('API (/v1) Open Endpoints Get Requests', () => {
-  describe('/campgrounds successful response', () => {
-    let response;
-    let resData;
+describe('Server Tests W/O  DB', () => {
+  const fakeDB = async () => {};
 
-    beforeAll(async () => {
-      response = await request(app).get('/v1/campgrounds');
-      resData = JSON.parse(response.text);
-    });
+  async function getApp() {
+    return await createApp(fakeDB);
+  }
 
-    test('correct status code', () => {
-      expect(response.statusCode).toBe(200);
-    });
+  let app;
 
-    test('header specifies json content type', () => {
-      expect(response.headers['content-type']).toEqual(
-        expect.stringContaining('json')
-      );
-    });
+  getApp().then(created => {
+    app = created;
+  });
 
-    test('response json key value pair of success:true', () => {
-      expect(resData.success).toEqual(true);
-    });
-
-    test('response json should contain count', () => {
-      expect(resData).toHaveProperty('count');
-    });
+  test('app starts and runs with fake db', async () => {
+    const response = await request(app).get('/');
+    expect(response.statusCode).toBe(404);
   });
 });
+
+// describe('API (/v1) Open Endpoints Get Requests', () => {
+
+//   describe('/campgrounds successful response', () => {
+//     let response;
+//     let resData;
+
+//     beforeAll(async () => {
+//       response = await request(app).get('/v1/campgrounds');
+//       resData = JSON.parse(response.text);
+//     });
+
+//     test('correct status code', () => {
+//       expect(response.statusCode).toBe(200);
+//     });
+
+//     test('header specifies json content type', () => {
+//       expect(response.headers['content-type']).toEqual(
+//         expect.stringContaining('json')
+//       );
+//     });
+
+//     test('response json key value pair of success:true', () => {
+//       expect(resData.success).toEqual(true);
+//     });
+
+//     test('response json should contain count', () => {
+//       expect(resData).toHaveProperty('count');
+//     });
+//   });
+// });
